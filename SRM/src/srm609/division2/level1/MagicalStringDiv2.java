cce2fd64c69416252cece6f6acb46d75affff10d
hide-san <hideo@sato.name>
2014-02-18 23:52:31 +0900

    My First GitHub check in!

---
+++ SRM/src/srm609/division2/level1/MagicalStringDiv2.java
@@ -0,0 +1,18 @@
+package srm609.division2.level1;
+
+/**
+ * Created by hideo on 2/18/14.
+ */
+public class MagicalStringDiv2 {
+    public int minimalMoves(String S) {
+        int move = 0;
+        for (int i = 0; i < S.length()/2; i++){
+            if (S.charAt(i) == '<') move++;
+        }
+
+        for (int i = S.length()/2; i < S.length(); i++){
+            if (S.charAt(i) == '>') move++;
+        }
+        return move ;
+    }
+}
---
+++ SRM/test/srm609/division2/level1/MagicalStringDiv2Test.java
@@ -0,0 +1,38 @@
+package srm609.division2.level1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by hideo on 2/18/14.
+ */
+public class MagicalStringDiv2Test {
+    @Test
+    public void example0() throws Exception {
+        srm609.division2.level1.MagicalStringDiv2 a = new MagicalStringDiv2();
+        int move = a.minimalMoves(">><<><");
+        Assert.assertEquals(2, move);
+
+    }
+    @Test
+    public void example1() throws Exception {
+        MagicalStringDiv2 a = new MagicalStringDiv2();
+        int move = a.minimalMoves(">>>><<<<");
+        Assert.assertEquals(0, move);
+
+    }
+    @Test
+    public void example2() throws Exception {
+        MagicalStringDiv2 a = new MagicalStringDiv2();
+        int move = a.minimalMoves("<<>>");
+        Assert.assertEquals(4, move);
+
+    }
+    @Test
+    public void example3() throws Exception {
+        MagicalStringDiv2 a = new MagicalStringDiv2();
+        int move = a.minimalMoves("<><<<>>>>><<>>>>><>><<<>><><><><<><<<<<><<>>><><><");
+        Assert.assertEquals(20, move);
+
+    }
+}
