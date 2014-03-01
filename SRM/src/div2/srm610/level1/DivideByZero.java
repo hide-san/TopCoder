d3c6fc59e0637311b6714d7366e6c23e31eac692
hide-san <hideo@sato.name>
2014-03-01 16:38:26 +0900

    SRM610

--- SRM/src/div2/srm610/level1/DivideByZero.java
+++ SRM/src/div2/srm610/level1/DivideByZero.java
@@ -1,38 +1,36 @@
 package div2.srm610.level1;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Vector;
+import java.util.*;
 
 /**
  * Created by hideo on 2/26/14.
  */
 public class DivideByZero {
     public int CountNumbers(int[] numbers) {
-        Vector<Integer> numberVector = new Vector<Integer>();
+        List numberList = new ArrayList();
         for (int i = 0; i < numbers.length; i++){
-            numberVector.add(numbers[i]);
+            numberList.add(numbers[i]);
         }
 
-        int count = numbers.length;
         int A, B, C;
 
-        for (int i = 0; i < numberVector.size(); i++) {
-            A = numberVector.get(i);
-            for (int j = 0; j < numberVector.size(); j++) {
-                B = numberVector.get(j);
+        for (int i = 0; i < numberList.size(); i++) {
+            A = (int) numberList.get(i);
+            for (int j = 0; j < numberList.size(); j++) {
+                B = (int) numberList.get(j);
                 System.out.println("A:" + A + "\tB:" + B);
 
                 if (A <= B) continue;
                 C = A / B;
                 System.out.println("A:" + A + "\tB:" + B + "\tC:" + C);
 
-                if (! numberVector.contains(C) ){
+                if (! numberList.contains(C) ){
                     System.out.println("ADD:" + C);
-                    count++;
-                    numberVector.add(C);
+                    numberList.add(C);
+                    i = -1;
+                    j = -1;
                 }
             }
         }
-        return count;
+        return numberList.size();
     }
 }
