9e8cdbb4d6672cae94660ca2d17e62d1038c5ced
hide-san <hideo@sato.name>
2014-10-11 00:27:15 +0900

    SRM634 level1

--- SRM/src/div2/srm634/level2/ShoppingSurveyDiv2.java
+++ SRM/src/div2/srm634/level2/ShoppingSurveyDiv2.java
@@ -1,17 +1,12 @@
-/**
- * Created by hideo on 14/09/28.
- */
 public class ShoppingSurveyDiv2 {
     public int minValue(int N, int[] s) {
         int missingItems = 0;
 
-        for (int i = 0; i < s.length; i++) {
-            missingItems = missingItems + (N - s[i]);
+        for (int value : s) {
+            missingItems = missingItems + (N - value);
         }
 
-        if (missingItems > N) return 0;
-
-        return N - missingItems;
+        return Math.max(0, N - missingItems);
 
     }
 }
