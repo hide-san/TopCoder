ce19b7754a962f73b0232f02b2435e0af6d50f63
hide-san <hideo@sato.name>
2014-12-02 00:39:52 +0900

    AliceGameEasy - another try (but it times out for some tests)

--- SRM 639 DIV 2/src/AliceGameEasy.java
+++ SRM 639 DIV 2/src/AliceGameEasy.java
@@ -2,55 +2,43 @@ public class AliceGameEasy {
     final long NO_ANSWER = -1;
 
     public long findMinimumValue(long x, long y) {
-        Score initScore = new Score();
-
-        long minimumValue = dfsRecursiveCall(initScore, x, y);
-        if (minimumValue == Long.MAX_VALUE) {
-            return NO_ANSWER;
-        } else {
-            return minimumValue;
-        }
-    }
-
-    public long dfsRecursiveCall(Score s, final long x, final long y) {
-        if (s.alicePoint > x || s.kiritoPoint > y) {
-            return Long.MAX_VALUE;
-        } else if (s.alicePoint == x && s.kiritoPoint == y) {
-            return s.aliceWinCount;
-        }
-
-        long retval = Long.MAX_VALUE;
-        for (Score nextScore : s.getNextScores()) {
-            long aliceWinCount = dfsRecursiveCall(nextScore, x, y);
-            if (aliceWinCount != Long.MAX_VALUE) {
-                retval = Math.min(retval, aliceWinCount);
+        long situation = 0;
+        Score score = new Score();
+        while (score.alicePoint <= x || score.kiritoPoint <= y) {
+            score = getScore(situation);
+            if (score.alicePoint == x && score.kiritoPoint == y) {
+                return score.aliceWinCount;
             }
-
+            situation++;
         }
-        return retval;
+
+        return NO_ANSWER;
     }
+
+    private Score getScore(long situation) {
+        Score score = new Score();
+
+        while (situation > 0) {
+            long aliceWinBit = situation % 2;
+            situation = situation / 2;
+
+            score.turn++;
+            if (aliceWinBit == 1L) {
+                score.alicePoint = score.alicePoint + score.turn;
+                score.aliceWinCount++;
+            } else {
+                score.kiritoPoint = score.kiritoPoint + score.turn;
+            }
+        }
+
+        return score;
+    }
+
 
     public class Score {
         long turn;
         long aliceWinCount;
         long alicePoint;
         long kiritoPoint;
-
-        private Score[] getNextScores() {
-            Score aliceWin = new Score();
-            aliceWin.turn = this.turn + 1;
-            aliceWin.aliceWinCount = this.aliceWinCount + 1;
-            aliceWin.alicePoint = this.alicePoint + aliceWin.turn;
-            aliceWin.kiritoPoint = this.kiritoPoint;
-
-            Score kirotoWin = new Score();
-            kirotoWin.turn = this.turn + 1;
-            kirotoWin.aliceWinCount = this.aliceWinCount;
-            kirotoWin.alicePoint = this.alicePoint;
-            kirotoWin.kiritoPoint = this.kiritoPoint + kirotoWin.turn;
-
-            return new Score[]{kirotoWin, aliceWin};
-        }
-
     }
 }
