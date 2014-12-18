3660e144fab2e3b465f7e77194ebe56bb5e3113c
hide-san <hideo@sato.name>
2014-12-18 16:44:34 +0900

    LightSwitchingPuzzle @ Single Round Match 641

--- SRM 642 DIV 2/src/LightSwitchingPuzzle.java
+++ SRM 642 DIV 2/src/LightSwitchingPuzzle.java
@@ -1,72 +1,36 @@
-import java.util.*;
-
 public class LightSwitchingPuzzle {
-    private final char ON = 'Y';
-    private final char OFF = 'N';
-    private final int NO_ANSWER = -1;
+    private static final char ON = 'Y';
+    private static final char OFF = 'N';
+    private static final int NO_ANSWER = -1;
 
     public int minFlips(String state) {
-        State initialState = new State();
-        initialState.state = state.toCharArray();
-        initialState.depth = 0;
+        int count = 0;
 
-        Queue<State> queue = new ArrayDeque<State>();
-        queue.offer(initialState);
-
-        while (!queue.isEmpty()) {
-            State thisState = queue.poll();
-            System.out.println("CurrentState" + Arrays.toString(thisState.state));
-            if (!new String(thisState.state).contains(String.valueOf(ON))) {
-                return thisState.depth;
-            }
-            for (State nextState : thisState.getChildrenBfs()) {
-                if (nextState != null) queue.offer(nextState);
+        for (int switchID = 1; switchID < state.length() + 1; switchID++) {
+            if (state.charAt(switchID - 1) == ON) {
+                count++;
+                state = getNextStae(state, switchID);
             }
         }
 
-        return NO_ANSWER;
+        if (state.contains("" + ON))
+            return NO_ANSWER;
+
+        return count;
     }
 
-    static List<String> statePatterns = new ArrayList<String>();
-
-    public class State {
-        public char[] state;
-        public int depth;
-
-        public State[] getChildrenBfs() {
-            final char[] currentState = state;
-
-            State[] childStates = new State[currentState.length - 1];
-            for (int i = 1; i < childStates.length + 1; i++) {
-                childStates[i - 1] = toggleSwitch(i);
-            }
-
-
-            return childStates;
-        }
-
-        public State toggleSwitch(int switchId) {
-            State nextState = new State();
-            nextState.state = new String(this.state).toCharArray();
-            nextState.depth = this.depth + 1;
-
-            for (int i = 0; i < nextState.state.length; i++) {
-                if ((i + 1) % switchId == 0) {
-                    if (nextState.state[i] == ON) {
-                        nextState.state[i] = OFF;
-                    } else {
-                        nextState.state[i] = ON;
-                    }
+    private String getNextStae(String state, int switchID) {
+        char[] charState = state.toCharArray();
+        for (int lightID = 1; lightID < state.length() + 1; lightID++) {
+            if (lightID % switchID == 0) {
+                if (charState[lightID - 1] == ON) {
+                    charState[lightID - 1] = OFF;
+                } else {
+                    charState[lightID - 1] = ON;
                 }
             }
-
-            if (statePatterns.contains(new String(nextState.state))) {
-                return null;
-            }
-
-            statePatterns.add(new String(nextState.state));
-            return nextState;
         }
-
+        return new String(charState);
     }
+
 }
