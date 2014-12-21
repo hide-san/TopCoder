fc8bd30a275d2552d0fd072be36e9933030ed65f
hide-san <hideo@sato.name>
2014-12-22 00:56:18 +0900

    NumberGameAgain @ Single Round Match 640

--- SRM 640 DIV 2/src/NumberGameAgain.java
+++ SRM 640 DIV 2/src/NumberGameAgain.java
@@ -2,29 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NumberGameAgain {
-    private final long INITIAL_NUMBER = 1;
-
-    private int K;
     private List<Long> TABLE = new ArrayList<Long>();
 
     public long solve(int k, long[] table) {
-        K = k;
-        for (long forbiddenNumber : table) {
-            TABLE.add(forbiddenNumber);
-        }
+        for (long forbiddenNumber : table) TABLE.add(forbiddenNumber);
 
-        long numbers = (long) Math.pow(2, K) - 1;
+        long numbers = (long) Math.pow(2, k) - 1;
         numbers -= 1; // No count "1"
 
         cleanupTable();
 
         for (long forbiddenNumber : TABLE) {
             int n = Math.getExponent((double) forbiddenNumber);
-            numbers -= (long) Math.pow(2, K - n) - 1;
+            numbers -= (long) Math.pow(2, k - n) - 1;
         }
 
         return numbers;
-
     }
 
     void cleanupTable() {
@@ -32,16 +25,13 @@ public class NumberGameAgain {
         for (Long i : TABLE) newTable.add(i);
 
         for (Long i : TABLE) {
-            boolean isRemoved = false;
-            int devideCont = 0;
             for (Long j : TABLE) {
 
-                Long temp = i;
-                while (temp >= 2) {
-                    temp = temp / 2;
-                    devideCont++;
-                    if (temp.equals(j)) {
-//                        System.out.println("i:" + i + "\tj:" + j + "\td:" + devideCont);
+                boolean isRemoved = false;
+                Long baseNumber = i;
+                while (baseNumber >= 2) {
+                    baseNumber = baseNumber / 2;
+                    if (baseNumber.equals(j)) {
                         newTable.remove(i);
                         isRemoved = true;
                     }
