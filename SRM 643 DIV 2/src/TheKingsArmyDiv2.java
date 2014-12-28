6c1c349373d138f94add5a070b460dffae11603a
hide-san <hideo@sato.name>
2014-12-28 20:23:18 +0900

    TheKingsArmyDiv2 @ Single Round Match 643

--- SRM 643 DIV 2/src/TheKingsArmyDiv2.java
+++ SRM 643 DIV 2/src/TheKingsArmyDiv2.java
@@ -4,27 +4,30 @@ public class TheKingsArmyDiv2 {
 
     public int getNumber(String[] state) {
         int countOfHappySolders = 0;
-        int R = state.length;
-        int C = state[0].length();
+        final int R = state.length;
+        final int C = state[0].length();
 
-        char[][] charState = new char[R + SHIFT * 2][C + SHIFT * 2];
         for (int row = 0; row < R; row++) {
             for (int column = 0; column < C; column++) {
-                charState[row + SHIFT][column + SHIFT] = state[row].charAt(column);
                 if (state[row].charAt(column) == HAPPY) countOfHappySolders++;
             }
         }
         if (countOfHappySolders == 0) return 2;
 
-        for (int row = SHIFT; row < R + SHIFT; row++) {
-            for (int column = SHIFT; column  < C + SHIFT; column++) {
-                if (charState[row][column] == HAPPY) {
-                    if (charState[row - 1][column] == HAPPY ||
-                            charState[row + 1][column] == HAPPY ||
-                            charState[row][column - 1] == HAPPY ||
-                            charState[row][column + 1] == HAPPY) return 0;
+        char[][] charState = new char[R + SHIFT * 2][C + SHIFT * 2];
+        for (int row = 0; row < R; row++) {
+            for (int column = 0; column < C; column++) {
+                charState[row + SHIFT][column + SHIFT] = state[row].charAt(column);
+            }
+        }
 
-                }
+        for (int row = SHIFT; row < R + SHIFT; row++) {
+            for (int column = SHIFT; column < C + SHIFT; column++) {
+                if (charState[row][column] == HAPPY && (charState[row - 1][column] == HAPPY ||
+                                                        charState[row + 1][column] == HAPPY ||
+                                                        charState[row][column - 1] == HAPPY ||
+                                                        charState[row][column + 1] == HAPPY)) return 0;
+
             }
         }
 
