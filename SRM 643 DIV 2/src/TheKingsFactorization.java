0fa5d9bbebe004697d87cf9580dfce89513213d2
hide-san <hideo@sato.name>
2014-12-28 21:24:29 +0900

    TheKingsFactorization @ SRM 643 DIV

--- SRM 643 DIV 2/src/TheKingsFactorization.java
+++ SRM 643 DIV 2/src/TheKingsFactorization.java
@@ -1,10 +1,38 @@
-import java.util.*;
-import java.math.*;
-import static java.lang.Math.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TheKingsFactorization {
-	
-	public long[] getVector(long N, long[] primes) {
-		return null;
-	}
+
+    public long[] getVector(long N, long[] primes) {
+        List<Long> answerList = new ArrayList<Long>();
+
+        for (int i = 0; i < primes.length; i++) {
+            answerList.add(primes[i]);
+            N = N / primes[i];
+
+            if (i == primes.length - 1) {
+                if (N != 1) answerList.add(N);
+                break;
+            }
+
+            if (N % primes[i] == 0) {
+                answerList.add(primes[i]);
+                N = N / primes[i];
+                continue;
+            }
+
+            for (long thisNumber = primes[i]; thisNumber <= N; thisNumber++) {
+                if (N % thisNumber == 0) {
+                    answerList.add(thisNumber);
+                    N = N / thisNumber;
+                    break;
+                }
+            }
+        }
+
+        long[] answerArray = new long[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) answerArray[i] = answerList.get(i);
+
+        return answerArray;
+    }
 }
