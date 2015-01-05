2e0b1bcfe54d9a6b4736ccfbbc31e68b0ee8b52f
hide-san <hideo@sato.name>
2015-01-06 01:28:43 +0900

    TheKingsFactorization @ SRM 643 DIV

---
+++ SRM 636 DIV 2/src/SortishDiv2.java
@@ -0,0 +1,84 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class SortishDiv2 {
+
+    private static final int ERASED = 0;
+
+    public int ways(int sortedness, int[] seq) {
+        int ways = 0;
+
+        for (int[] thisSeq : getPossibleSeq(seq)) {
+            if (getSortedness(thisSeq) == sortedness) {
+                ways++;
+            }
+        }
+
+        return ways;
+    }
+
+    private int getSortedness(final int[] thisSeq) {
+        int sortedness = 0;
+
+        for (int i = 0; i < thisSeq.length; i++) {
+            for (int j = i + 1; j < thisSeq.length; j++) {
+                if (thisSeq[i] < thisSeq[j]) sortedness++;
+            }
+        }
+
+        return sortedness;
+    }
+
+    private List<int[]> getPossibleSeq(final int[] seq) {
+        List<int[]> possibleSeq = new ArrayList<int[]>();
+
+        Queue<int[]> queue = new ArrayDeque<int[]>();
+        queue.offer(seq);
+        while (!queue.isEmpty()) {
+            int[] thisSeq = queue.poll();
+            if (!hasSpecificSeq(thisSeq, ERASED)) {
+                possibleSeq.add(thisSeq);
+                continue;
+            }
+            for (int[] nextSeq : getNextSeq(thisSeq)) {
+                queue.offer(nextSeq);
+            }
+        }
+
+        return possibleSeq;
+    }
+
+    private boolean hasSpecificSeq(final int[] seq, final int targetSeq) {
+        for (int thisSeq : seq) {
+            if (thisSeq == targetSeq) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private List<int[]> getNextSeq(final int[] seq) {
+        List<int[]> nextSeqList = new ArrayList<int[]>();
+
+        for (int i = 0; i < seq.length; i++) {
+            if (seq[i] == ERASED) {
+                for (int j = 1; j < seq.length + 1; j++) {
+                    if (hasSpecificSeq(seq, j)) {
+                        continue;
+                    }
+
+                    int[] nextSeq = new int[seq.length];
+                    System.arraycopy(seq, 0, nextSeq, 0, seq.length);
+                    nextSeq[i] = j;
+                    nextSeqList.add(nextSeq);
+                }
+                break;
+            }
+        }
+
+        return nextSeqList;
+    }
+
+}
